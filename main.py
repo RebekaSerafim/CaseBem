1d5b19759917dbb3745738f8f6cd25f026ff90b8
Otavia <otaviamonteiro09@gmail.com>
2025-08-12 16:56:58 -0300

    comit

--- main.py
+++ main.py
@@ -15,16 +15,23 @@ async def get_root():
 
 @app.get("/produtoseservicos")
 async def get_root():
-    response = templates.TemplateResponse("produtoseservicos.html", {"request": {}})
+    response = templates.TemplateResponse("produtoseservicos/produtoseservicos.html", {"request": {}})
     return response
 
-@app.get("produtoseservicos_vestidos")
+@app.get("/produtoseservicos_vestidos")
 async def get_root():
-    response = templates.TemplateResponse("produtoseservicos_vestidos.html", {"request": {}})
+    response = templates.TemplateResponse("produtoseservicos/produtoseservicos_vestidos/produtoseservicos_vestidos.html", {"request": {}})
     return response
 
+@app.get("/card_vestido1")
+async def get_root():
+    response = templates.TemplateResponse("produtoseservicos/produtoseservicos_vestidos/card_vestido1.html", {"request": {}})
+    return response
 
-
+@app.get("/card_vestido2")
+async def get_root():
+    response = templates.TemplateResponse("produtoseservicos/produtoseservicos_vestidos/card_vestido2.html", {"request": {}})
+    return response
 
 if __name__ == "__main__":
     uvicorn.run(app="main:app", host="127.0.0.1", port=8000, reload=True)
