06469d6680d1812619901822ba17f3adf64e1a5e
Otavia <otaviamonteiro09@gmail.com>
2025-08-20 17:53:18 -0300

    ok

--- main.py
+++ main.py
@@ -2,8 +2,12 @@ from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 import uvicorn
 
-from routes import locador_routes, noivo_routes, public_routes
+from routes import locador_routes 
 from routes import usuario_routes
+from routes import prestador_routes
+from routes import fornecedor_routes
+from routes import noivo_routes
+from routes import public_routes
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
@@ -12,6 +16,8 @@ app.include_router(public_routes.router)
 app.include_router(usuario_routes.router)
 app.include_router(locador_routes.router)
 app.include_router(noivo_routes.router)
+app.include_router(prestador_routes.router)
+app.include_router(fornecedor_routes.router)
 
 if __name__ == "__main__":
     uvicorn.run(app="main:app", host="127.0.0.1", port=8000, reload=True)
