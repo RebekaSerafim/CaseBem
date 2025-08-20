35c3679c4c9f5deac0d040591a0807df303913f6
Rebeka <rebekaviserafim@gmail.com>
2025-08-20 17:49:51 -0300

    provável derrota part.2

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 import uvicorn
 
-from routes import public_routes
+from routes import locador_routes, noivo_routes, public_routes
 from routes import usuario_routes
 
 app = FastAPI()
@@ -10,6 +10,8 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 
 app.include_router(public_routes.router)
 app.include_router(usuario_routes.router)
+app.include_router(locador_routes.router)
+app.include_router(noivo_routes.router)
 
 if __name__ == "__main__":
     uvicorn.run(app="main:app", host="127.0.0.1", port=8000, reload=True)
